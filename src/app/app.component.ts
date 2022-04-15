cb0c2d1c9ded7a6e72f9471ed7fef274f5e096ce
maida-f <maida.filipovic@stu.ssst.edu.ba>
2022-04-15 18:56:03 +0200

    modified code for aboutus page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ɵgetDOM} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './aboutus2/app.component.html',
+  styleUrls: ['./aboutus2/app.component.css']
 })
 export class AppComponent {
   title = 'application';
