044509e771631b1513bf47ac7bee38808babf3e4
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-06-02 23:44:04 +0200

    Added: frontend by Maida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AboutUs} from './aboutus/app.component';
+import {LoginComponent} from './login/login.component';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {Contact_us} from "./contact_us/contact_us";
+import {AboutusComponent} from "./aboutus/aboutus.component";
 
 
 
 const routes: Routes = [
 
-  { path: 'about', component: AboutUs },
+  { path: '', component: LoginComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'contact', component: Contact_us },
+  { path: 'about', component: AboutusComponent },
+
+
 
 ];
 
