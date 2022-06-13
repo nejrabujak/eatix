74fd26fbbebb8ee4c0ce90758f7abc59e98466ed
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-06-13 17:08:23 +0200

    Updated environment variables

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Store} from '../models/store';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable()
 export class StoreService {
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  backendUrl: 'https://git.heroku.com/app-eatix.git',
+};
