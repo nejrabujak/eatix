6bafa3d54bad0074a4d7dfb2520444c6e89ecf31
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-06-15 15:04:48 +0200

    Run on localhost

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Store} from '../models/store';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment.prod';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class StoreService {
